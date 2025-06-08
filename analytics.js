a592bd833e0c6bf496b7d8a99d7991bd7a27d0df
Oliver Koh <140325819+oliverkohal@users.noreply.github.com>
2025-06-08 21:41:46 +0800

    Delete analytics.js

--- analytics.js
+++
@@ -1,8 +0,0 @@
-// Vercel Analytics - Load when page is ready
-(function() {
-    // Create and append the Vercel analytics script
-    const script = document.createElement('script');
-    script.defer = true;
-    script.src = '/_vercel/insights/script.js';
-    document.head.appendChild(script);
-})();
